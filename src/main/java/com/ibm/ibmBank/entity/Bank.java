158ec25fa20c625b63e8ebdfd845579be89ddaf5
unknown <ibmjavanh16@iiht.tech>
2021-05-04 15:38:31 +0530

    created bank entity

--- src/main/java/com/ibm/ibmBank/entity/Bank.java
+++ src/main/java/com/ibm/ibmBank/entity/Bank.java
@@ -1,5 +1,40 @@
 package com.ibm.ibmBank.entity;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class Bank {
 
+	@NotNull
+	private String ifscCode;
+	@NotNull
+	@NotBlank
+	private String name;
+	@NotNull
+	@NotBlank
+	private String branchName;
+
+	public String getIfscCode() {
+		return ifscCode;
+	}
+
+	public void setIfscCode(String ifscCode) {
+		this.ifscCode = ifscCode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBranchName() {
+		return branchName;
+	}
+
+	public void setBranchName(String branchName) {
+		this.branchName = branchName;
+	}
 }
